d8c7f56ac3ed354678c9b73627ff619977c4228d
Mike <entopologist@gmail.com>
2018-12-09 01:32:55 +0300

    Help comments edit

--- Fader/Fader.cs
+++ Fader/Fader.cs
@@ -1,13 +1,15 @@
 ﻿/*
  
+	This is mass fade-in, fade-out creation script for Vegas Pro that allows to create and remove fades, as well as change the curves of the existing ones.
+
     Set desired fade length in milliseconds of frames and click OK button.
 
     If you want to create crossfades with specific curve types tick the "Change Curve Type" checkbox.
-    You can also change curve type of existing events or remove fades by selecting the appropiate mode from the drop down list.
+    You can also change curve type of existing events or remove fades by selecting the appropriate mode from the drop down list.
 
     Press "I" and "O" to quick check/uncheck FadeIn and FadeOut checkboxes.
 
-    The script can operate in non-intecactive mode, without GUI.
+    The script can operate in non-interactive mode, without GUI.
     To do so change script's settings below and set Settings.ShowDialog to false and tune other settings to your needs.
     This way you can copy the script to a new file with custom settings and run it triggered by a shortcut key.
 
